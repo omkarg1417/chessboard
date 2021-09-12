b39130460230f0d257ee001798e3da8b77331549
omkarg1417 <omkarg1417@gmail.com>
2021-09-13 01:51:06 +0530

    ChessBoard complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+ 
 function App() {
+
+  function Cell() {
+    return (
+      <span className="cell"></span>
+    )
+  }
+
+  function GenerateBoard() {
+    let list =[];
+    let tempStyle = {
+      display: "none",
+    }
+    for(let i = 0; i < 8; ++i) {
+        for(let j = 0; j < 8; ++j) {
+            list.push(<Cell/>);
+        }
+        list.push(<span style={tempStyle}></span>);
+    }
+    return list;
+  }
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="board">
+      <GenerateBoard/>
     </div>
   );
 }
